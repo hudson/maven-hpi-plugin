5a9df9d1a3cadf40d620d78df514e08bee49ecb5
Winston Prakash <winston.prakash@gmail.com>
2011-08-04 15:09:08 -0700

    Add the EPL license Header to the appropriate classes for eclipse move

--- src/main/java/org/jvnet/hudson/maven/plugins/hpi/HpiMojo.java
+++ src/main/java/org/jvnet/hudson/maven/plugins/hpi/HpiMojo.java
@@ -1,20 +1,19 @@
-package org.jvnet.hudson.maven.plugins.hpi;
+/*******************************************************************************
+ *
+ * Copyright (c) 2004-2009 Oracle Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: 
+ *
+ *    Kohsuke Kawaguchi  
+ *
+ *******************************************************************************/ 
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package org.jvnet.hudson.maven.plugins.hpi;
 
 import org.apache.maven.archiver.MavenArchiveConfiguration;
 import org.apache.maven.archiver.MavenArchiver;
