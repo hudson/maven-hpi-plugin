2f8d4d32e343fe6e698e0b877694dc163e3945f6
Winston Prakash <winston.prakash@gmail.com>
2012-03-08 16:35:26 -0800

    Fix: hpi:create fails with Velocity Template error (http://jira.codehaus.org/browse/ARCHETYPE-246). The fix is to escape "#"

--- hpi-archetype/src/main/java/HelloWorldBuilder.java
+++ hpi-archetype/src/main/java/HelloWorldBuilder.java
@@ -19,17 +19,15 @@ import java.io.IOException;
  *
  * <p>
  * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * {@link DescriptorImpl\#newInstance(StaplerRequest)} is invoked
  * and a new {@link HelloWorldBuilder} is created. The created
  * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
+ * XStream, so this allows you to use instance fields (like {@link \#name})
  * to remember the configuration.
  *
  * <p>
- * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)} method
+ * When a build is performed, the {@link \#perform(AbstractBuild, Launcher, BuildListener)} method
  * will be invoked. 
- *
- * @author Kohsuke Kawaguchi
  */
 public class HelloWorldBuilder extends Builder {
 
